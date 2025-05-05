ad9d6556f61a1f6fd491d93401453985c3a94420
Srikar2610 <srikarpadakanti10@gmail.com>
2025-05-05 12:35:41 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "my_ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "my_instance_type" {
+  description = "Instance type for the EC2 instance"
+  type        = string
+}
