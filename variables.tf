36f7099b6923d92139961b2060eb3ff91c3f0707
Srikar2610 <srikarpadakanti10@gmail.com>
2025-05-05 12:57:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "my_ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-}
-
-variable "my_instance_type" {
-  description = "Instance type for the EC2 instance"
-  type        = string
-}
+  variable "my_ami_id" {
+  type = string
+  //default = "ami-0c02fb55956c7d316"
+  }
+  variable "my_instance_type" {
+   type=string
+   //default = "t3.medium"
+  }
+ 
