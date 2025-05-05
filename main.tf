6a5aeabe76102cca90c32a486ea28306630c343b
Srikar2610 <srikarpadakanti10@gmail.com>
2025-05-05 13:12:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "sample_inst"{
-  ami= var.my_ami_id
-  instance_type = var.my_instance_type
+resource "aws_instance" "modul-inst" {
+    ami= var.my-ami
+    instance_type = var.my-inst-type
 }
