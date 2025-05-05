f9c44fc0dcbdcc6a9b9f18cddf9359e8960ddc4d
Srikar2610 <srikarpadakanti10@gmail.com>
2025-05-05 12:57:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-resource "aws_instance" "my_instance" {
-  ami           = var.my_ami_id
+resource "aws_instance" "sample_inst"{
+  ami= var.my_ami_id
   instance_type = var.my_instance_type
-
-  tags = {
-    Name = "MyEC2FromModule"
-  }
 }
